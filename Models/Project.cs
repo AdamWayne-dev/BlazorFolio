4c392c890a36b65dbc7ad1d4ff8c13274ac62426
Adam Wayne <adamwayne.dev@Gmail.com>
2025-07-15 15:45:41 +0100

    Main body commit

---
+++ Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazor_Portfolio_Website.Models
+{
+    public class Project
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string ProjectUrl { get; set; } = string.Empty;
+        public string GitHubUrl { get; set; } = string.Empty;
+        public List<string> Technologies { get; set; } = new List<string>();
+    }
+}
---
+++ ProjectService.cs
@@ -0,0 +1,33 @@
+﻿using Blazor_Portfolio_Website.Models;
+
+namespace Blazor_Portfolio_Website.Services
+{
+    public class ProjectService
+    {
+        public List<Project> GetProjects()
+        {
+            return new List<Project>
+            {
+                new Project
+                {
+                    Name = "Portfolio website",
+                    Description = "A portfolio that contains all of the projects I've completed as well as the ones I'm currently working on.",
+                    ImageUrl = "project/images.png",
+                    ProjectUrl = "placeholder.com",
+                    GitHubUrl = "placeholder.com",
+                    Technologies = new List<string> { "C#", "Blazor", "HTML", "CSS" }
+                },
+
+                new Project
+                {
+                    Name = "Handsome Cow's Arcade of Classics",
+                    Description = "A collection of classic arcade games that can be played in the browser.",
+                    ImageUrl = "project/arcade.png",
+                    ProjectUrl = "www.placeholder.com",
+                    GitHubUrl = "www.placeholder.com",
+                    Technologies = new List<string> {"Blender","Aseprite","Unity", "C#"}
+                }
+            };
+        } 
+    }
+}
