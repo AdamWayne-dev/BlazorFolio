cf58e67413fa81bd5b0e796716ce24e4082ced39
Adam Wayne <adamwayne.dev@Gmail.com>
2025-07-15 16:45:25 +0100

    Added portfolio page

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
 using Blazor_Portfolio_Website;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazor_Portfolio_Website.Services;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+public class Program 
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddSingleton<ProjectService>();// Register the ProjectService as a singleton, which is appropriate for static data.
 
-await builder.Build().RunAsync();
+        await builder.Build().RunAsync();
+    }
+}
