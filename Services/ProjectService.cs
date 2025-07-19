48056553aba664f47d178394c34672a0a0d849f4
Adam Wayne <adamwayne.dev@Gmail.com>
2025-07-19 18:03:50 +0100

    Added "About me" page and edited the CSS styles to match the rest of the website.

--- Services/ProjectService.cs
+++ Services/ProjectService.cs
@@ -12,9 +12,9 @@ namespace Blazor_Portfolio_Website.Services
                 {
                     Name = "Portfolio website",
                     Description = "A portfolio that contains all of the projects I've completed as well as the ones I'm currently working on.",
-                    ImageUrl = "project/images.png",
-                    ProjectUrl = "placeholder.com",
-                    GitHubUrl = "placeholder.com",
+                    ImageUrl = "",
+                    ProjectUrl = "",
+                    GitHubUrl = "https://github.com/AdamWayne-dev/BlazorFolio",
                     Technologies = new List<string> { "C#", "Blazor", "HTML", "CSS" }
                 },
 
